061cc91f726d1ce06637c2508b577b4f99070d0e
Iain McFarlane <32712706+imcfarla2003@users.noreply.github.com>
2019-01-07 22:09:04 +0000

    Update Docs and version number

--- plugin.py
+++ plugin.py
@@ -1,5 +1,5 @@
 '''
-<plugin key="HivePlug2" name="Hive Plugin2" author="imcfarla, MikeF and roadsnail" version="0.9(Dev)" wikilink="http://www.domoticz.com/wiki/plugins" externallink="https://github.com/imcfarla2003/domoticz-hive">
+<plugin key="HivePlug" name="Hive Plugin" author="imcfarla, MikeF and roadsnail" version="1.0" wikilink="http://www.domoticz.com/wiki/plugins" externallink="https://github.com/imcfarla2003/domoticz-hive">
     <description>
         <h2>Hive Plugin</h2>
         <h3>Features</h3>
